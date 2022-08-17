ace8989fd5cec7b96dfce633709df080888bfd33
Trent <trent.moca@gmail.com>
2022-08-17 16:06:43 -0700

    Add timestamp to data collected
    Also set frequency at 10 Hz and organize code into functions

--- read_imu_write_sd.ino
+++ read_imu_write_sd.ino
@@ -4,8 +4,12 @@
 #include <DFRobot_BMX160.h>
 
 const int chipSelect = 4;
-DFRobot_BMX160 bmx160;
+
 const String FILE_NAME = "imudata.txt";
+
+long microsStart = 0;
+long logCount = 0;
+DFRobot_BMX160 bmx160;
 
 void setup() {
   delay(100);
@@ -13,7 +17,7 @@ void setup() {
   // set up SD
   // see if the card is present and can be initialized:
   if (!SD.begin(chipSelect)) {
-    while (1);
+    while(1);
   }
 
   // set up IMU
@@ -39,26 +43,58 @@ void setup() {
 }
 
 void loop() {
+  /* Initialize time start for data timestamps */
+  if (microsStart == 0){
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    microsStart = micros();
+    String dataLog = getDataLog();
+    uploadString(dataLog);
+    logCount++;
+  }
+
+  if ((micros() - microsStart)/100000 >= logCount) {
+    String dataLog = getDataLog();
+    uploadString(dataLog);
+    logCount++;
+  }
+
+}
+
+String getDataLog() {
   sBmx160SensorData_t Omagn, Ogyro, Oaccel;
-  /* Get a new sensor event */
   bmx160.getAllData(&Omagn, &Ogyro, &Oaccel);
 
-  String dataString = "\t null\t null\t";
+  String dataString = "\t null\t" + readableDateTime() + "\t";
   dataString += String(Oaccel.x) + "\t" + String(Oaccel.y) + "\t" + String(Oaccel.z) + "\t";
   dataString += String(Ogyro.x) + "\t" + String(Ogyro.y) + "\t" + String(Ogyro.z) + "\t";
   dataString += "\t\t\t\t";
-  dataString += String(Omagn.x) + "\t" + String(Omagn.y) + "\t" + String(Omagn.z) + "\t";
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);              
+  dataString += String(Omagn.x) + "\t" + String(Omagn.y) + "\t" + String(Omagn.z) + "\t"; 
+  return dataString;
+}
 
+void uploadString(String data) {
   File dataFile = SD.open(FILE_NAME, FILE_WRITE);
   if (dataFile) {
-    dataFile.println(dataString);
+    dataFile.println(data);
     dataFile.close();
   }
+}
+
+String readableDateTime() {
+  double totalSeconds = (micros() - microsStart)/1000000.0;
+  int totalMinutes = floor(totalSeconds)/60;
+  int totalHours = floor(totalSeconds)/3600;
+  int totalDays = floor(totalSeconds)/86400;
+  double secondsDouble = totalSeconds - totalMinutes*60;
+  int seconds = floor(secondsDouble);
+  int minutes = totalMinutes - totalHours*60;
+  int hours = totalHours - totalDays*24;
+  String yearMonth = " 1970-01-";
+  char dateTimeCharArray[19];
+  int deciseconds = round((secondsDouble - seconds)*1000000);
+  sprintf(dateTimeCharArray, "%02d %02d:%02d:%02d.%06d", totalDays, hours, minutes, seconds, deciseconds);
   
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-  
-  
+  return yearMonth + String(dateTimeCharArray); 
 }
