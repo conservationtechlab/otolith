3eca86a381c0cc667a30a6a4db9442b349cf424b
Trent <trent.moca@gmail.com>
2022-09-13 16:40:44 -0700

    Update log data
    -Fix bug that the day is the 0th of the month.
    -Allow the user to insert their own delimiter, so file output can
    easily be changed from .tsv to .csv
    -Move start time blink to before timers are initialized (cannot
    implement delay within an interrupt)

--- read_imu_write_sd/read_imu_write_sd.ino
+++ read_imu_write_sd/read_imu_write_sd.ino
@@ -5,11 +5,12 @@
 #include <DFRobot_BMX160.h>
 
 
-#define TIMER_INTERVAL_20HZ       50L
-#define TIMER_INTERVAL_1HZ        1000L
+#define TIMER_INTERVAL_READ       50L
+#define TIMER_INTERVAL_WRITE      1000L
 #define HW_TIMER_INTERVAL_MS      10
 #define CHIP_SELECT               4
-#define FILE_NAME                 "intrupt1.txt"
+#define FILE_NAME                 "intrupt2.txt"
+String  DELIM =                   "\t";
 
 long microsStart = 0;
 DFRobot_BMX160 bmx160;
@@ -34,18 +35,27 @@ void setup() {
   if (ITimer.attachInterruptInterval_MS(HW_TIMER_INTERVAL_MS, TimerHandler)){
     ;
   }
-  ISR_Timer.setInterval(TIMER_INTERVAL_20HZ,  readingImuData);
-  ISR_Timer.setInterval(TIMER_INTERVAL_1HZ,  uploadingImuData);
+
+  /* Initialize time start for data timestamps */
+  if (microsStart == 0){
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    microsStart = micros();
+  }
+  
+  ISR_Timer.setInterval(TIMER_INTERVAL_READ,  readingImuData);
+  ISR_Timer.setInterval(TIMER_INTERVAL_WRITE, uploadingImuData);
 
   // open file
   File dataFile = SD.open(FILE_NAME, FILE_WRITE);
   if (dataFile) {
     String timeStamp = "StartTime: TODO";
-    String fieldHeader = "address\tTime(s)\tChipTime";
-    fieldHeader += "\tax(m/s^2)\tay(m/s^2)\taz(m/s^2)";
-    fieldHeader += "\twx(g)\twy(g)\twz(g)";
-    fieldHeader += "\tAngleX(deg)\tAngleY(deg)\tAngleZ(deg)\tT(°)";
-    fieldHeader += "\thx(uT)\thy(uT)\thz(uT)";
+    String fieldHeader = "address" + DELIM + "Time(s)" + DELIM + "ChipTime";
+    fieldHeader += DELIM + "ax(m/s^2)" + DELIM + "ay(m/s^2)" + DELIM + "az(m/s^2)";
+    fieldHeader += DELIM + "wx(g)" + DELIM + "wy(g)" + DELIM + "wz(g)";
+    fieldHeader += DELIM + "AngleX(deg)" + DELIM + "AngleY(deg)" + DELIM + "AngleZ(deg)" + DELIM + "T(°)";
+    fieldHeader += DELIM + "hx(uT)" + DELIM + "hy(uT)" + DELIM + "hz(uT)";
     dataFile.println(timeStamp);
     dataFile.println(fieldHeader);
     dataFile.close();
@@ -63,13 +73,6 @@ void TimerHandler(void){
 
 
 void readingImuData() {
-  /* Initialize time start for data timestamps */
-  if (microsStart == 0){
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(100);
-    digitalWrite(LED_BUILTIN, LOW);
-    microsStart = micros();
-  }
   flushableString += getDataLog() + "\n";
 }
 
@@ -77,15 +80,13 @@ void readingImuData() {
 void uploadingImuData(){
   String tempString = flushableString;
   if (tempString.length() == flushableString.length()){
-    flushableString = "";
+    flushableString = "";  // Without this check, race conditions were more likely
   }
   else {
     uploadingImuData();
     return;
   }
-  digitalWrite(LED_BUILTIN, HIGH);
   uploadString(tempString);
-  digitalWrite(LED_BUILTIN, LOW);
 }
 
 
@@ -93,11 +94,11 @@ String getDataLog() {
   sBmx160SensorData_t Omagn, Ogyro, Oaccel;
   bmx160.getAllData(&Omagn, &Ogyro, &Oaccel);
 
-  String dataString = "\t null\t" + readableDateTime() + "\t";
-  dataString += String(Oaccel.x) + "\t" + String(Oaccel.y) + "\t" + String(Oaccel.z) + "\t";
-  dataString += String(Ogyro.x) + "\t" + String(Ogyro.y) + "\t" + String(Ogyro.z) + "\t";
-  dataString += "\t\t\t\t";
-  dataString += String(Omagn.x) + "\t" + String(Omagn.y) + "\t" + String(Omagn.z) + "\t"; 
+  String dataString = DELIM + " null" + DELIM + readableDateTime() + DELIM;
+  dataString += String(Oaccel.x) + DELIM + String(Oaccel.y) + DELIM + String(Oaccel.z) + DELIM;
+  dataString += String(Ogyro.x) + DELIM + String(Ogyro.y) + DELIM + String(Ogyro.z) + DELIM;
+  dataString += DELIM + DELIM + DELIM + DELIM;
+  dataString += String(Omagn.x) + DELIM + String(Omagn.y) + DELIM + String(Omagn.z) + DELIM; 
   return dataString;
 }
 
@@ -115,7 +116,7 @@ String readableDateTime() {
   double totalSeconds = (micros() - microsStart)/1000000.0;
   int totalMinutes = floor(totalSeconds)/60;
   int totalHours = floor(totalSeconds)/3600;
-  int totalDays = floor(totalSeconds)/86400;
+  int totalDays = 1 + floor(totalSeconds)/86400; //add 1 because January 0th isn't valid
   double secondsDouble = totalSeconds - totalMinutes*60;
   int seconds = floor(secondsDouble);
   int minutes = totalMinutes - totalHours*60;
@@ -123,7 +124,8 @@ String readableDateTime() {
   String yearMonth = " 1970-01-";
   char dateTimeCharArray[19];
   int deciseconds = round((secondsDouble - seconds)*1000000);
-  sprintf(dateTimeCharArray, "%02d %02d:%02d:%02d.%06d", totalDays, hours, minutes, seconds, deciseconds);
+  sprintf(dateTimeCharArray, "%02d %02d:%02d:%02d.%06d", totalDays, hours, minutes, seconds, deciseconds); 
+      //after one month, the date stops being valid -- perhaps include long of time along with human-readable
   
   return yearMonth + String(dateTimeCharArray); 
 }
