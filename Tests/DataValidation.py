285150f863ae8971914c8db835d5d00500699090
hongyuejin <hojin@ucsd.edu>
2024-04-08 11:43:24 -0700

    Added more tests to the data validation

--- Tests/DataValidation.py
+++ Tests/DataValidation.py
@@ -1,15 +1,28 @@
 # This is a data validation test script
 # Author: Grace Jin
 # Input the acquired csv file (not the interpolated) and test if the data is correctly collected
-
+# The script will check the following:
+# 1. Column names
+# 2. Average time intervals
+# 3. Largest timeshift
+# 4. Duplicated rows
+# 5. Real time clock
+# 6. System time clock
+# 7. Missing values
+# 8. Missing data points
+# 9. Frequency distribution of time intervals
+# The script will print out the issues found and a summary of the validation test
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime as dt
 
 EXPECTED_COLUMNS = ['SystemTime(ms)', 'RealTime', 'AccelX', 'AccelY', 'AccelZ', 'GyroX', 'GyroY', 'GyroZ', 'MagX', 'MagY', 'MagZ']
 EXPECTED_INTERVAL = 50  # in ms
+FMT = "%H:%M:%S"
 
 
 def validate_data(filename):
+    # Read the csv file and insert an index column
     df = pd.read_csv(filename)
     issues = []
     print("----------Validating Data----------")
@@ -34,14 +47,39 @@ def validate_data(filename):
     if df.duplicated().any():
         issues.append(f"There are {df.duplicated().sum()} duplicated rows in the data.")
     else:
-        print("Correct - No duplicated rows with same timestamps found.")
-    # 5. Check for missing values
+        print("Correct - No duplicated rows found.")
+
+    # 5. Check if the real time is correctly incremented
+    df['RealTime'] = pd.to_datetime(df['RealTime'], format=FMT).dt.time
+    # convert the real time to seconds
+    realtime_seconds = df['RealTime'].apply(lambda x: dt.strptime(str(x), FMT).timestamp())
+    # print(realtime_seconds)
+    timedeltas = realtime_seconds.diff().dropna()
+    # count how many negative time intervals happened
+    failed_count = sum(timedeltas < 0)
+    if failed_count > 0:
+        issues.append("Real time clock is not correctly incremented. System might had: " + str(failed_count) + " fails.")
+    else:
+        print("Correct - Real time clock is correctly incremented.")
+    # print(df['RealTime'])
+
+    # 6. Check if the system time is correctly incremented
+    timedeltas = df['SystemTime(ms)'].diff().dropna()
+    # count how many negative time intervals happened
+    reset_count = sum(timedeltas < 0)
+    if reset_count > 0:
+        issues.append("System time is not correctly incremented. System might had: " + str(reset_count) + " resets.")
+    else:
+        print("Correct - System time is correctly incremented.")
+
+    # 6. Check for missing values
     missing_values_count = df.isna().sum().sum()
     if missing_values_count > 0:
         issues.append(f"There are {missing_values_count} missing values in the data.")
     else:
         print("Correct - No timestamps are logged without sensor data.")
-    # 6. Report missing datapoints
+
+    # 7. Report missing datapoints
     duration = df['SystemTime(ms)'].iloc[-1] - df['SystemTime(ms)'].iloc[0]
     expected_data_points = duration / EXPECTED_INTERVAL
     missing_data_points = expected_data_points - len(df)
@@ -49,7 +87,7 @@ def validate_data(filename):
         issues.append(
             f"Missing {int(missing_data_points)} data points. Expected {int(expected_data_points)} but got {len(df)}.")
 
-    # 7. Frequency distribution of time intervals
+    # 8. Frequency distribution of time intervals
     frequency_dist = time_diffs.value_counts()
     # Histogram for frequency distribution
     plt.figure(figsize=(10, 6))
