4c96799a0c1d01f423e5c0f243021f9c0d1dba5c
hongyuejin <hojin@ucsd.edu>
2024-04-15 09:30:43 -0700

    Added the lora info page

---
+++ Otolith20Hz/lorainfo.h
@@ -0,0 +1,4 @@
+// Replace with keys obtained from ChirpStack 
+#define SECRET_DEV_ADDR "xxxxxxxx"
+#define SECRET_NWK_KEY "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#define SECRET_APP_KEY "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
